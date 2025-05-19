20406a0bdf85b93f3bf5cb8bb88a405f324a950c
Lorenzo Antei <lorenzoantei@protonmail.com>
2025-05-19 18:21:31 +0200

    added footer

---
+++ src/components/Footer.svelte
@@ -0,0 +1,5 @@
+<footer class="py-6 mx-24 flex flex-row justify-start">
+    <div><a href="/">Lorenzo Antei</a>&nbsp;~&nbsp;</div>
+    <div> PI 0123456789012&nbsp;~&nbsp;</div>
+    <div><a href="https://t.me/l0r3nz010" target="_blank"> telegram</a></div>
+</footer>
--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,5 +1,9 @@
 <script>
   let { children } = $props();
   import "../app.css";
+  import Footer from '../components/Footer.svelte'; // Assicurati che il path sia corretto
 </script>
+
 {@render children()}
+
+<Footer />
